81bedceffb9a17f00555d4bc277fe2ec34b4425b
João Mangueira <jamjs7@gmail.com>
2022-12-07 17:11:04 -0300

    primeiro commit do projeto

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>João Portifolio</title>
+      </Head>
+
+        <h1 className="text-blue-800">Lets build an AWESOME portifolio</h1>
+    </div>
+  )
+}
