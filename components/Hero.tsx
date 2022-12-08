4846e4b41fc0347f8e0d375f2604e307eaacc8bd
João Mangueira <jamjs7@gmail.com>
2022-12-08 17:07:49 -0300

    compenent: BackgroundCircles

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
-import { SocialIcon } from 'react-social-icons';
-import { motion } from 'framer-motion';
+import { Cursor, useTypewriter } from 'react-simple-typewriter';
+import BackgroundCircles from './BackgroundCircles';
 
 export default function Hero() {
+    const [text, count] = useTypewriter({
+        words: [
+            "Hi, the name's João Mangueira",
+            "Guy-who-loves-football.tsx",
+            "<ButLovesToCodeMore />",
+        ],
+        loop: true,
+        delaySpeed: 2000,
+    });
     return (
-        <div>Hero</div>
+        <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+            <BackgroundCircles />
+            <img src="" alt="" />
+            <h1>
+                <span>{text}</span>
+                <Cursor cursorColor="#F7AB0A" />
+            </h1>
+        </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Hero from "../components/Hero";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>João Portifolio</title>
       </Head>
@@ -12,7 +12,7 @@ export default function Home() {
         {/* Header */}
         <Header />
         {/* Hero */}
-        <section id="hero">
+        <section id="hero" className="snap-center">
             <Hero />
         </section>
         {/* About */}
