84af30f2e1ec9ea7fa7fd016cbb05254818a4506
João Mangueira <jamjs7@gmail.com>
2022-12-08 16:36:41 -0300

    compenent: Hero

---
+++ components/Hero.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { SocialIcon } from 'react-social-icons';
+import { motion } from 'framer-motion';
+
+export default function Hero() {
+    return (
+        <div>Hero</div>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Header from '../components/Header';
+import Hero from "../components/Hero";
 
 export default function Home() {
   return (
@@ -11,7 +12,9 @@ export default function Home() {
         {/* Header */}
         <Header />
         {/* Hero */}
-
+        <section id="hero">
+            <Hero />
+        </section>
         {/* About */}
 
         {/* Experience */}
