54c2818e27bef402a89e4b018108450dfc66ec07
João Mangueira <jamjs7@gmail.com>
2022-12-09 20:24:50 -0300

    add motion

--- components/About.tsx
+++ components/About.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import { Cursor, useTypewriter } from 'react-simple-typewriter';
-import BackgroundCircles from './BackgroundCircles';
-import Link from "next/link";
 import { motion } from 'framer-motion';
 
 export default function About() {
--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
-import { Cursor, useTypewriter } from 'react-simple-typewriter';
-import BackgroundCircles from './BackgroundCircles';
-import Link from "next/link";
 import { motion } from 'framer-motion';
 
 export default function Experience() {
     return (
-       <div>
-           WorkExperience
-       </div>
+       <motion.div
+           initial={{
+               opacity: 0,
+           }}
+           transition={{
+               duration: 1.5,
+           }}
+           whileInView={{
+               opacity: 1,
+           }}
+           className="h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center"
+       >
+           <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+               WorkExperience
+           </h3>
+
+           <div>
+
+           </div>
+       </motion.div>
     )
 }
