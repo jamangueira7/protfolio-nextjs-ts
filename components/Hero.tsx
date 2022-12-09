998ce0ae65806b7eef86862cffd679f22d5b556c
João Mangueira <jamjs7@gmail.com>
2022-12-09 19:46:20 -0300

    Hero com foto e css. Links tbm adicionados

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Cursor, useTypewriter } from 'react-simple-typewriter';
 import BackgroundCircles from './BackgroundCircles';
+import Link from "next/link";
 
 export default function Hero() {
     const [text, count] = useTypewriter({
@@ -13,13 +14,37 @@ export default function Hero() {
         delaySpeed: 2000,
     });
     return (
-        <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+        <div
+            className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden"
+        >
             <BackgroundCircles />
-            <img src="" alt="" />
-            <h1>
-                <span>{text}</span>
+            <img
+                className="relative rounded-full h-32 w-32 mx-auto object-center"
+                src="https://avatars.githubusercontent.com/u/1902749"
+                alt=""
+            />
+            <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">Software Engineer</h2>
+            <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+                <span className="mr-3">{text}</span>
                 <Cursor cursorColor="#F7AB0A" />
             </h1>
+            <div>
+                <Link href="#about">
+                    <button className="heroButton">About</button>
+                </Link>
+
+                <Link href="#experience">
+                    <button className="heroButton">Experience</button>
+                </Link>
+
+                <Link href="#skills">
+                    <button className="heroButton">Skills</button>
+                </Link>
+
+                <Link href="#projects">
+                    <button className="heroButton">Projects</button>
+                </Link>
+            </div>
         </div>
     )
 }
