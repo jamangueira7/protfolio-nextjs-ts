b9c97d7e433a68e2285e18fa5568732dd52d4f11
João Mangueira <jamjs7@gmail.com>
2022-12-09 20:15:10 -0300

    Finalizando componente about

--- components/About.tsx
+++ components/About.tsx
@@ -6,7 +6,16 @@ import { motion } from 'framer-motion';
 
 export default function About() {
     return (
-        <div
+        <motion.div
+            initial={{
+                opacity: 0,
+            }}
+            transition={{
+                duration: 1.5
+            }}
+            whileInView={{
+                opacity: 1,
+            }}
             className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
         >
             <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
@@ -31,10 +40,15 @@ export default function About() {
 
                 src="https://avatars.githubusercontent.com/u/1902749"
                 className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"
-            >
-
-            </motion.img>
-
-        </div>
+            />
+            <div className="space-y-10 px-0 md:px-10">
+                <h4 className="text-4xl font-semibold">
+                    Here is a <span className="underline decoration-[#F7AB0A]">little</span> background
+                </h4>
+                <p className="text-sm">
+                    Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.
+                </p>
+            </div>
+        </motion.div>
     )
 }
