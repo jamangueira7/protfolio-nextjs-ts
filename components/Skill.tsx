0695bdc03a8be595c1027e401a62b123f6cabdc2
João Mangueira <jamjs7@gmail.com>
2022-12-12 10:56:51 -0300

    finalizando Skills

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,20 +1,32 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import ExperienceCard from './ExperienceCard';
 
-export default function Skills() {
+type Props = {
+    directionLeft?: boolean;
+};
+
+export default function Skill({ directionLeft }: Props) {
     return (
-       <motion.div className="flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center">
-           <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl"
-           >
-               Skills
-           </h3>
-           <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm">
-               Hover over a skill for currency proficiency
-           </h3>
-           <div className="grid grid-cols-4 gap-5">
-                
-           </div>
-       </motion.div>
+        <div className="group relative flex cursor-pointer">
+            <motion.img
+                initial={{
+                    x: directionLeft ? -200 : 200,
+                    opacity: 0,
+                }}
+                transition={{
+                    duration: 1.5,
+                }}
+                whileInView={{
+                    opacity: 1,
+                }}
+                src="https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_96102ac6497377cd53da621075fe828e/sanity.png"
+                className="rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
+            />
+            <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0">
+                <div className="flex items-center justify-center h-full">
+                    <p className="text-3xl font-bold text-black opacity-100">100%</p>
+                </div>
+            </div>
+        </div>
     )
 }
