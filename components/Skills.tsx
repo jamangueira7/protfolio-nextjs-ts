ed82d25789d30e4be30cfbf521f366aaccb55803
João Mangueira <jamjs7@gmail.com>
2022-12-12 10:33:03 -0300

    Novo Component:Skill

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import ExperienceCard from './ExperienceCard';
 
-export default function Skills() {
+export default function Skill() {
     return (
-       <div className="h-screen">
-           Skills
+       <div className="grid grid-cols-4 gap-5">
+            Skill
        </div>
     )
 }
